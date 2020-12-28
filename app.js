5e99b9a89a25145dd623c7c1a085dead973b7729
matteeyao <myaoski@gmail.com>
2020-12-27 20:20:41 -0800

    Revise app.js

--- app.js
+++ app.js
@@ -21,7 +21,14 @@ const tweets = require("./routes/api/tweets");
 
 const passport = require('passport');
 
+const path = require('path');
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
 
 // Connect MongoDB using Mongoose:
 mongoose
